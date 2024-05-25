1f716a5dc36b6c33f253a8eefbc5c063aa5c453f
“Kseorang1218” <switch1218@uos.ac.kr>
2024-05-26 00:40:54 +0900

    feat : 그래프 그리는 코드 추가
    추후 리팩토링 예정

---
+++ generated_graphs/30204_IR_600RPM/get_graph.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+filename = os.getcwd().split('\\')[4]
+
+
+def get_graph(csv_file : str):
+
+    file = csv_file  # 파일 경로를 지정하세요.
+    df = pd.read_csv(file, usecols=[0, 1, 2, 3], names=['A', 'B', 'C', 'D'], header=0)  # 첫 4개 열만 불러옵니다.
+
+    # 각 열의 평균을 구합니다.
+    means = df.mean()
+
+
+    # 각 열의 모든 값에서 각 열의 평균을 뺍니다.
+    df_centered = df - means
+
+    for column in df_centered.columns:
+        plt.figure(figsize=(15, 6))
+        plt.plot(df_centered.index/120, df_centered[column]*0.0722222)
+        plt.title(f'Centered Values of {column}', size=15)
+        plt.xlabel('Time[s]', size=15)
+        plt.ylabel('Displacement[mm]', size=15)
+        plt.savefig(f'{filename}_{column}_{csv_file[0].upper()}_axis.png')
+        plt.close()
+
+    print("Graphs saved successfully.")
+
+get_graph('y.csv')
+get_graph('z.csv')
