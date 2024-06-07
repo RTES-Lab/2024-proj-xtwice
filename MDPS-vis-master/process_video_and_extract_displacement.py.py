42a8abb35072c43f43d32b7d03bb1c80fd8223d1
“Kseorang1218” <switch1218@uos.ac.kr>
2024-06-07 19:51:36 +0900

    feat : 변위 추출 한번에 실행하는 코드
    i: int(지점 번호)
    bearing_type: str(베이링 종류)

---
+++ MDPS-vis-master/process_video_and_extract_displacement.py.py
@@ -0,0 +1,6 @@
+import os
+
+i = 2
+bearing_type = "B"
+os.system(f"python ./example/get_roi.py -fname ./input/0530_30204_{bearing_type}_1200RPM_120fps_{i}.mov -f 120 -o ./test/0530_30204_{bearing_type}_1200RPM_120fps_{i}.json")
+os.system(f"python ./example/extract_displacement.py -fname ./input/0530_30204_{bearing_type}_1200RPM_120fps_{i}.mov -f 120 -skip 0 -o ./output/0530_30204_{bearing_type}_1200RPM_120fps_{i} -fo 14 -flb 0.01 -fub 1.0 -a 0 -roi ./test/0530_30204_{bearing_type}_1200RPM_120fps_{i}.json ")
