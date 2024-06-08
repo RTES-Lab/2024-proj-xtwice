208951c0a0c87a728a831893985e0cbdc32cb204
“Kseorang1218” <switch1218@uos.ac.kr>
2024-06-08 23:53:53 +0900

    ✏️ style : 파일 이름 오타 수정

--- MDPS-vis-master/process_video_and_extract_displacement.py
+++ MDPS-vis-master/process_video_and_extract_displacement.py
@@ -7,10 +7,10 @@
 import os
 
 # 영상 위치 번호
-i: int = 7
+i: int = 1
 
 # 영상에 사용된 베어링 타입
-bearing_type: str = "IR"
+bearing_type: str = "OR"
 
 os.system(f"python ./example/get_roi.py -fname ./input/0530_30204_{bearing_type}_1200RPM_120fps_{i}.mov -f 120 -o ./test/0530_30204_{bearing_type}_1200RPM_120fps_{i}.json")
 os.system(f"python ./example/extract_displacement.py -fname ./input/0530_30204_{bearing_type}_1200RPM_120fps_{i}.mov -f 120 -skip 0 -o ./output/0530_30204_{bearing_type}_1200RPM_120fps_{i} -fo 14 -flb 0.01 -fub 1.0 -a 0 -roi ./test/0530_30204_{bearing_type}_1200RPM_120fps_{i}.json ")
