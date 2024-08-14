2cfd8207b6c9bd419395bbbe9ef5ebcdaa5624ce
“Kseorang1218” <switch1218@uos.ac.kr>
2024-08-14 18:04:26 +0900

    refactoring : 학습 코드 수정
    PyTorch Lightning 적용

---
+++ MDPS-vis-master/model/funs/plmodule.py
@@ -0,0 +1,69 @@
+# plmodule.py
+
+import pytorch_lightning as pl
+import torch
+
+from torchmetrics.functional import accuracy
+
+class PlModule(pl.LightningModule):
+    def __init__(self, model, optimizer, criterion):
+        super().__init__()
+        self.model = model
+        self.optimizer = optimizer
+        self.criterion = criterion
+
+    def forward(self, x):
+        return self.model(x)
+    
+    def training_step(self, batch, batch_idx):
+        data, label = batch
+        data = data.unsqueeze(1)
+        output = self(data)
+        loss = self.criterion(output, label)
+
+        prediction = torch.argmax(output, dim=1)
+        # correct += prediction.eq(label.view_as(prediction)).sum().item()
+        acc = accuracy(prediction, label)
+
+        self.log('train_loss', loss, prog_bar=True, on_step=False, on_epoch=True)
+        self.log('train_acc', acc, prog_bar=True, on_step=False, on_epoch=True)
+
+        return loss
+    
+    def evaluate(self, batch, stage=None):
+        data, label = batch
+        output = self(data)
+        loss = self.criterion(output, label)
+
+        prediction = torch.argmax(output, dim=1)
+        # correct += prediction.eq(label.view_as(prediction)).sum().item()
+        acc = accuracy(prediction, label)
+
+        if stage:
+            self.log(f'{stage}_loss', loss, prog_bar=True, on_step=False, on_epoch=True)
+            self.log(f'{stage}_acc', acc, prog_bar=True, on_step=False, on_epoch=True)
+
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        return self.evaluate(batch=batch, stage="val")
+
+    def test_step(self, batch, batch_idx):
+        return self.evaluate(batch=batch, stage="test")
+    
+    def configure_optimizers(self):
+        optimizer = self.optimizer
+        return [optimizer]
+    
+
+class MetricsLogger(pl.Callback):
+    def __init__(self):
+        super().__init__()
+        self.train_losses = []
+        self.train_accuracies = []
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        self.train_losses.append(trainer.callback_metrics["train_loss"].item())
+        self.train_accuracies.append(trainer.callback_metrics["train_acc"].item())
+
+    
--- MDPS-vis-master/model/main.py
+++ MDPS-vis-master/model/main.py
@@ -5,9 +5,11 @@ from funs.databuilder import *
 from funs.dataset import *
 from funs.processing import *
 from funs.model import *
-from funs.trainer import *
+from funs.plmodule import *
 
 from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 # load yaml
 config = load_yaml('./model_config.yaml')
@@ -20,18 +22,18 @@ else:
     DEVICE = torch.device('cpu')
 
 
+sticker_label = f'{config.sticker_label}data'
+
+
 output_root = os.path.join(config.output_dir, config.date)
 
 df = make_dataframe(output_root, config)
 
-Adata, Alabel = build_from_dataframe(df, config.sample_length, config.shift, data_column='Adata', is_onehot=False)
-Bdata, Blabel = build_from_dataframe(df,  config.sample_length, config.shift, data_column='Bdata', is_onehot=False)
+data, label = build_from_dataframe(df, config.sample_length, config.shift, data_column=sticker_label, is_onehot=False)
 
-train_dataset_A = NumpyDataset(data=Adata, label=Alabel, transform=NpToTensor(), target_transform=NpToTensor())
-train_dataset_B = NumpyDataset(data=Bdata, label=Blabel, transform=NpToTensor(), target_transform=NpToTensor())
+train_dataset = NumpyDataset(data=data, label=label, transform=NpToTensor(), target_transform=NpToTensor())
 
-train_loader_A = DataLoader(dataset=train_dataset_A, batch_size=config.batch_size, shuffle=True, drop_last=False)
-train_loader_B = DataLoader(dataset=train_dataset_B, batch_size=config.batch_size, shuffle=True, drop_last=False)
+train_loader = DataLoader(dataset=train_dataset, batch_size=config.batch_size, shuffle=True, drop_last=False, num_workers=3)
 
 model = Simple1DCNN(num_classes=4).to(DEVICE)
 
@@ -39,28 +41,26 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 loss = torch.nn.CrossEntropyLoss()
 
 
-train_acc_list = []
-train_loss_list = []
+training_module = PlModule(model, optimizer, loss)
 
-for epoch in range(1, config.epoch + 1):
-    train_loss, train_accuracy = train(model, train_loader_A, optimizer, DEVICE, loss, config.batch_size)
-    train_acc_list.append(train_accuracy)
-    train_loss_list.append(train_loss)
-    print("[EPOCH: {}] \tTrain Loss: {:.4f}, \tTrain Accuracy: {:.4f}".format(
-        epoch, train_loss, train_accuracy))
-    
-plot_training_curves(train_loss_list, train_acc_list, data='A')
+ckpt_callback = ModelCheckpoint(
+    dirpath='./saved',
+    filename=f"{sticker_label[0]}_{config.epoch:02d}",
+    monitor = 'train_loss',
+    save_top_k = 1,
+    mode = 'min'
+)
 
+metrics_logger = MetricsLogger()
 
-train_acc_list = []
-train_loss_list = []
+trainer = pl.Trainer(
+    accelerator="gpu",
+    max_epochs=config.epoch,
+    callbacks=[ckpt_callback, metrics_logger],
+    log_every_n_steps=1,
+    default_root_dir="./saved",
+    )
 
-for epoch in range(1, config.epoch + 1):
-    train_loss, train_accuracy = train(model, train_loader_B, optimizer, DEVICE, loss, config.batch_size)
-    train_acc_list.append(train_accuracy)
-    train_loss_list.append(train_loss)
-    print("[EPOCH: {}] \tTrain Loss: {:.4f}, \tTrain Accuracy: {:.4f}".format(
-        epoch, train_loss, train_accuracy))
+trainer.fit(training_module, train_loader)
 
-
-plot_training_curves(train_loss_list, train_acc_list, data='B')
+plot_training_curves(metrics_logger.train_losses, metrics_logger.train_accuracies, data=sticker_label[0])
