a3aafb8eebbb3f8b11183ecb686ebebb715b0bef
Kseorang1218 <switch1218@uos.ac.kr>
2024-12-03 15:25:59 +0900

    design: ui 변경
    ui 깔끔하게 변경, 입력한 값을 예측에 사용하도록 변경

--- app/src/main/java/com/example/modelapp/MainActivity.kt
+++ app/src/main/java/com/example/modelapp/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.modelapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.modelapp.model.MyModel
 
@@ -15,24 +17,35 @@ class MainActivity : AppCompatActivity() {
 
         model = MyModel(this)
 
+        val inputValue: EditText = findViewById(R.id.inputValue)
         val predictButton: Button = findViewById(R.id.predictButton)
         val resultText: TextView = findViewById(R.id.resultText)
 
         predictButton.setOnClickListener {
-            // 입력값 설정 (FloatArray로 1개 값을 사용)
-            val input = FloatArray(1) {  0.0208934f }
+            val inputText = inputValue.text.toString()
 
-            // 예측 실행
-            val output = model.predict(input)
+            if (inputText.isBlank()) {
+                Toast.makeText(this, "Please enter a value", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
-            // 클래스별 확률 출력
-            val probabilities = output.joinToString(separator = ", ") { "%.4f".format(it) }
-            val maxProbabilityIndex = output.indices.maxByOrNull { output[it] } ?: -1
-            val classes = arrayOf("B", "H", "IR", "OR")
-            val predictedClass = if (maxProbabilityIndex != -1) classes[maxProbabilityIndex] else "Unknown"
+            try {
+                val input = FloatArray(1) { inputText.toFloat() }
 
-            resultText.text = "Class Probabilities: [$probabilities]\nPredicted Class: $predictedClass"
+                // 예측 실행
+                val output = model.predict(input)
 
+                // 클래스별 확률 출력
+                val probabilities = output.joinToString(separator = ", ") { "%.4f".format(it) }
+                val maxProbabilityIndex = output.indices.maxByOrNull { output[it] } ?: -1
+                val classes = arrayOf("B", "H", "IR", "OR")
+                val predictedClass =
+                    if (maxProbabilityIndex != -1) classes[maxProbabilityIndex] else "Unknown"
+
+                resultText.text = "Class Probabilities: [$probabilities]\nPredicted Class: $predictedClass"
+            } catch (e: NumberFormatException) {
+                Toast.makeText(this, "Invalid input. Please enter a valid number.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
